1fd796e695a036d89d1dbac1d18b6975d4813b0a
elevateforhumanity <elevateforhumanity@gmail.com>
2025-09-07 03:40:10 +0000

    Your commit message

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+function App() {
+  return <h1>Vite + React is working!</h1>;
+}
+
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
+
+// ➜  Local:   http://localhost:5173/
